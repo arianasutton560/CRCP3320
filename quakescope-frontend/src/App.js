ebb7adc12b5068a072a968909a07e3cf176281f1
Ariana Sutton <agsutton@smu.edu>
2025-10-07 15:59:27 -0500

    edit scripts

--- quakescope-frontend/src/App.js
+++ quakescope-frontend/src/App.js
@@ -1,5 +1,3 @@
-// This file holds the links to your different pages
-
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -9,13 +7,13 @@ function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/visualdata">Visual Data</Link>
+        <Link to="/">Home</Link> | <Link to="/About">About</Link> | <Link to="/VisualData">Visual Data</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/visualdata" element={<VisualData />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/VisualData" element={<VisualData />} />
       </Routes>
     </Router>
   );
